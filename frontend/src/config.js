1ebfbf0221beabba2b4fc6db7db508c4f97f629b
Kartone <estarpro1022@163.com>
2024-11-11 13:32:16 +0800

    deploy url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const SERVER_ENDPOINT = "http://localhost:5005";
+// export const SERVER_ENDPOINT = "http://localhost:5005";
+
+export const SERVER_ENDPOINT = "https://z5440480-presto.vercel.app:5005";
