4e862881928719c4cd24b2298c5f515cd01f121b
Kartone <estarpro1022@163.com>
2024-11-11 17:08:00 +0800

    server endpoint

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const SERVER_ENDPOINT = "http://localhost:5005";
+// export const SERVER_ENDPOINT = "http://localhost:5005";
 
-// export const SERVER_ENDPOINT = "https://z5440480-presto.vercel.app";
+export const SERVER_ENDPOINT = "https://z5440480-presto.vercel.app";
